04f2443fcfaed946b18152b9389e645c811f0757
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-17 11:49:20 +0000

    Exercicio 2

---
+++ ArraySums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero : ");
+            int LV =Convert.ToInt32(Console.ReadLine());
+
+           
+
+            int[ , ] matrix = new int[ LV , LV ] ;
+            for (int i = 0; i<matrix.GetLength(0);i++){
+                
+                for (int j = 0; j<matrix.GetLength(1);j++){
+                    Console.WriteLine("Numero da posição ("+i+","+j+")");
+                    int nCL=Console.Read();
+                    
+                   // Console.Write(matrix[i,j]+" ");
+                   
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
