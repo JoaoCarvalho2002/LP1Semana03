c9ca8cdbcf5626b6fd984398cf60edf9643aacbc
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-17 10:22:36 +0000

    Exercicio 1, Completo

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write something!");
+
+            string S=Console.ReadLine();
+
+            Console.WriteLine("Write the character to be removed!");
+
+            string C=Console.ReadLine();
+        
+            char C2=Convert.ToChar(C);
+            
+            for(int i =0;i<S.Length;i++){
+                if (S[i]!=C2){
+                    Console.WriteLine(S[i]);
+
+                }
+                
+
+            } 
+        }
+    }
+}
