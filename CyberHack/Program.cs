089b23325a58cf5e96f9745507f6f7f5be6871d4
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-03-05 15:59:03 +0000

    Add cyberhack to solution and add enums

---
+++ CyberHack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CyberHack
+{
+    public class Program
+    {
+        public enum HackTool
+        {
+            BruteForce,
+            Phishing,
+            Backdoor,
+            ZeroDay,
+            AIOverride,
+            QuantumExploit
+        }
+        public enum SystemType
+        {
+            CorporateServer,
+            BankDatabase,
+            SmartCityCore,
+            MilitaryAI
+        }
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello LP!");
+
+
+        }
+    }
+}
